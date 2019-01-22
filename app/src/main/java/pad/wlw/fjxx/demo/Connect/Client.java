ddb6ae71ae4a7de97909f84709516b54f4507f0e
lhf <954356394@qq.com>
2019-01-22 20:12:19 +0800

    多文件测试

--- app/src/main/java/pad/wlw/fjxx/demo/Connect/Client.java
+++ app/src/main/java/pad/wlw/fjxx/demo/Connect/Client.java
@@ -4,8 +4,10 @@ import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Environment;
+import android.telephony.mbms.FileInfo;
 import android.util.Log;
 
+import java.io.BufferedInputStream;
 import java.io.DataInputStream;
 import java.io.File;
 import java.io.FileOutputStream;
@@ -15,13 +17,25 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class Client extends Socket {
-    private static final String SERVER_IP = "192.168.0.100";
+    private static String SERVER_IP = "192.168.0.100";
     private static final int SERVER_PORT = 8899;
     private DataInputStream dis;
     private FileOutputStream fos;
     private Socket client;
     private SharedPreferences SP;
     private SharedPreferences.Editor editor;
+
+    @SuppressLint("CommitPrefEdits")
+    public Client(Context context, String IP) throws IOException {
+        super(SERVER_IP, SERVER_PORT);
+        SERVER_IP = IP;
+        this.client = this;
+        Log.i("Seccess", "成功连接到服务器");
+        this.setKeepAlive(true);
+        SP = context.getSharedPreferences("Save", Context.MODE_PRIVATE);
+        editor = SP.edit();
+        save();
+    }
 
     @SuppressLint("CommitPrefEdits")
     public Client(Context context) throws IOException {
@@ -31,7 +45,12 @@ public class Client extends Socket {
         this.setKeepAlive(true);
         SP = context.getSharedPreferences("Save", Context.MODE_PRIVATE);
         editor = SP.edit();
-        saveFile(1);
+        save();
+    }
+
+    private void save() {
+//        saveFile(1);
+        receiveFile(client);
     }
 
     /**
@@ -43,7 +62,6 @@ public class Client extends Socket {
         try {
             dis = new DataInputStream(this.getInputStream());
             String fileName = dis.readUTF();
-
             File file = new File(Environment.getExternalStorageDirectory().toString() + "/" + fileName);
 //            @SuppressLint("SdCardPath") File file = new File("/sdcard/" + (pathType == 1 ? "Photo/" : "Movie/")
 //                    + fileName);
@@ -61,16 +79,113 @@ public class Client extends Socket {
         } catch (IOException e) {
             e.printStackTrace();
         } finally {
-//            try {
-//                if (fos != null)
-//                    fos.close();
-//                if (dis != null)
-//                    dis.close();
-//                this.close();
-//            } catch (IOException e) {
-//                e.printStackTrace();
-//            }
+            try {
+                if (fos != null)
+                    fos.close();
+                if (dis != null)
+                    dis.close();
+                this.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
+    }
+
+    class mFileInfo {
+        String mFileName;
+        long mFileSize;
+    }
+
+    protected void receiveFile(Socket socket) {
+        File dirs = new File(Environment.getExternalStorageDirectory().toString() + "/");
+        if (!dirs.exists()) {
+            dirs.mkdirs();
+        }
+        DataInputStream din = null;
+        int fileNum = 0;
+        long totalSize = 0;
+        mFileInfo[] fileinfos = null;
+        try {
+            din = new DataInputStream(new BufferedInputStream(
+                    socket.getInputStream()));
+            fileNum = din.readInt();
+            fileinfos = new mFileInfo[fileNum];
+            for (int i = 0; i < fileNum; i++) {
+                fileinfos[i] = new mFileInfo();
+                fileinfos[i].mFileName = din.readUTF();
+                fileinfos[i].mFileSize = din.readLong();
+            }
+            totalSize = din.readLong();
+        } catch (IOException e) {
+            e.printStackTrace();
+            Log.d("IO错误", "readInt Exception");
+            System.exit(0);
+        }
+        System.out.println(fileNum);
+        System.out.println(totalSize);
+        for (mFileInfo fileinfo : fileinfos) {
+            System.out.println(fileinfo.mFileName);
+            System.out.println(fileinfo.mFileSize);
+        }
+        // // /////////////////////////////////////////////////////////////////
+        int leftLen = 0; // 写满文件后缓存区中剩余的字节长度。
+        int bufferedLen = 0; // 当前缓冲区中的字节数
+        int writeLen = 0; // 每次向文件中写入的字节数
+        long writeLens = 0; // 当前已经向单个文件中写入的字节总数
+        long totalWriteLens = 0; // 写入的所有字节数
+        byte buf[] = new byte[8192];
+        byte temp[];  //缓存区部分内容    缓冲对应System.arraycopy
+        for (int i = 0; i < fileNum; i++) {
+            writeLens = 0;
+            try {
+//                FileOutputStream fout = new FileOutputStream(dirs
+//                        + fileinfos[i].mFileName);
+                @SuppressLint("SdCardPath") FileOutputStream fout = new FileOutputStream("/sdcard/"
+                        + fileinfos[i].mFileName);
+                while (true) {
+                    if (leftLen > 0) {
+                        bufferedLen = leftLen;
+                    } else {
+                        bufferedLen = din.read(buf);
+                    }
+                    if (bufferedLen == -1)
+                        return;
+                    System.out.println("readlen" + bufferedLen);
+                    // 如果已写入文件的字节数加上缓存区中的字节数已大于文件的大小，只写入缓存区的部分内容。
+                    if (writeLens + bufferedLen >= fileinfos[i].mFileSize) {
+                        leftLen = (int) (writeLens + bufferedLen - fileinfos[i].mFileSize);
+                        writeLen = bufferedLen - leftLen;
+                        fout.write(buf, 0, writeLen); // 写入部分
+                        totalWriteLens += writeLen;
+//                        move(buf, writeLen, leftLen);
+                        //代替上面那个move的作用
+                        temp = new byte[8192];
+//                        System.arraycopy(temp, 0, buf, writeLen, leftLen);
+                        System.arraycopy(temp, 0, buf, bufferedLen - leftLen, bufferedLen);
+                        buf = temp;
+                        break;
+                    } else {
+                        fout.write(buf, 0, bufferedLen); // 全部写入
+                        writeLens += bufferedLen;
+                        totalWriteLens += bufferedLen;
+                        if (totalWriteLens >= totalSize) {
+                            //mListener.report(GroupChatActivity.FAIL, null);
+                            return;
+                        }
+                        leftLen = 0;
+                    }
+                    //mListener.report(GroupChatActivity.PROGRESS,
+                    //(int) (totalWriteLens * 100 / totalSize));
+                } // end while
+                fout.close();
+
+            } catch (Exception e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+                Log.d("接收文件失败", "receive file Exception");
+            }
+        } // end for
+        //mListener.report(GroupChatActivity.FAIL, null);
     }
 
     private void saveState(int pathType, Set<String> files) {
--- app/src/main/java/pad/wlw/fjxx/demo/MainActivity.java
+++ app/src/main/java/pad/wlw/fjxx/demo/MainActivity.java
@@ -2,20 +2,44 @@ package pad.wlw.fjxx.demo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import java.io.IOException;
 
 import pad.wlw.fjxx.demo.Connect.Client;
 import pad.wlw.fjxx.demo.Util.SD_Permission;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private Button btn_accept;
+    private EditText et_ip;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initView();
+        //检测APP有没有SD卡的读写权限
         SD_Permission.verifyStoragePermissions(this);
-        Thread thread = new Thread() {
+//        Con();
+    }
+
+    private void Con(final String IP) {
+        new Thread() {
+            @Override
+            public void run() {
+                try {
+                    Client client = new Client(MainActivity.this, IP);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }.start();
+    }
+
+    private void Con() {
+        new Thread() {
             @Override
             public void run() {
                 try {
@@ -24,7 +48,26 @@ public class MainActivity extends AppCompatActivity {
                     e.printStackTrace();
                 }
             }
-        };
-        thread.start();
+        }.start();
+    }
+
+    private void initView() {
+        btn_accept = findViewById(R.id.btn_accept);
+        et_ip = findViewById(R.id.et_ip);
+        btn_accept.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn_accept:
+                String IP = et_ip.getText().toString();
+                if (!IP.equals("")) {
+                    Con(IP);
+                } else {
+                    Con();
+                }
+                break;
+        }
     }
 }
